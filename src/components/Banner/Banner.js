e713003659e89ec20712fa874a33f369a6e85cf1
Mariadelaluz9814 <20041251@itdurango.edu.mx>
2024-04-16 09:27:41 -0600

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,13 @@
+import "./Banner.css"
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>Marco Tuxedo</h1>
+            <p>Renta de trajes a la medida</p>
+        </div>
+    </div>
+  );
+};
+
+export default Banner
